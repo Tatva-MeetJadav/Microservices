eb7d1626b206c604f4d1038954eccdd2be9c84be
Tatva-MeetJadav <tatva.pct169@outlook.com>
2025-07-31 12:15:28 +0530

    set the default value in example schema

--- src/Microservices/Startup.cs
+++ src/Microservices/Startup.cs
@@ -171,7 +171,7 @@ namespace Microservices
                     // set route prefix to openapi, e.g. http://localhost:8080/openapi/index.html
                     c.RoutePrefix = "swagger";
                     //TODO: Either use the SwaggerGen generated OpenAPI contract (generated from C# classes)
-                    c.SwaggerEndpoint("/openapi/1.0.0/openapi.json", "Email Verification API");
+                    c.SwaggerEndpoint("/openapi-original.json", "Email Verification API");
 
                     //TODO: Or alternatively use the original OpenAPI contract that's included in the static files
                     // c.SwaggerEndpoint("/openapi-original.json", "Email Verification API Original");
