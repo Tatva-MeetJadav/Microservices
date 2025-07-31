d5c7630f71ec797392053bc868db54f3ac8efbf6
Tatva-MeetJadav <tatva.pct169@outlook.com>
2025-07-31 10:50:13 +0530

    updated route to swagger from openapi

--- src/Microservices/Startup.cs
+++ src/Microservices/Startup.cs
@@ -169,12 +169,13 @@ namespace Microservices
                 .UseSwaggerUI(c =>
                 {
                     // set route prefix to openapi, e.g. http://localhost:8080/openapi/index.html
-                    c.RoutePrefix = "openapi";
+                    c.RoutePrefix = "swagger";
                     //TODO: Either use the SwaggerGen generated OpenAPI contract (generated from C# classes)
                     c.SwaggerEndpoint("/openapi/1.0.0/openapi.json", "Email Verification API");
 
                     //TODO: Or alternatively use the original OpenAPI contract that's included in the static files
                     // c.SwaggerEndpoint("/openapi-original.json", "Email Verification API Original");
+
                 });
             app.UseRouting();
             app.UseEndpoints(endpoints =>
